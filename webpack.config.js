4b4683ac4be8f2d2a6a17c3d17ca46921a2067e4
Abangpa1ace <dyek72731@gmail.com>
2022-09-27 22:57:12 +0900

    env 테스트

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  name: "webpack-test",
-  mode: "development",
-  devtool: "eval",
-  resolve: {
-    extensions: [".js", ".jsx", ".tsx", ".ts"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+module.exports = (env) => {
+  return {
+    name: "webpack-test",
+    mode: "development",
+    devtool: "eval",
+    resolve: {
+      extensions: [".js", ".jsx", ".tsx", ".ts"],
+    },
+    module: {
+      rules: [
+        // "babel-loader",
+        {
+          test: /\.(ts|tsx)$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    entry: "./src/index.tsx",
+    output: {
+      filename: "app.js",
+      path: path.join(__dirname, "dist"),
+      publicPath: "/dist/",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html",
+      }),
     ],
-  },
-  entry: {
-    app: "./src/index.tsx",
-  },
-  output: {
-    filename: "app.js",
-    path: path.join(__dirname, "dist"),
-    publicPath: "/dist/",
-  },
-  devServer: {
-    hot: true,
-    port: 3001,
-  },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "src"),
+      },
+      contentBase: path.resolve(__dirname, "./dist"),
+      compress: true,
+      hot: true,
+      port: 3001,
+      liveReload: true,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
 };
